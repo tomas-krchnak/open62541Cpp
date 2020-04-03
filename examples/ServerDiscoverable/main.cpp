46e791104686fc39c7dbca172f3c8e8b8698e6f3
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-04-03 19:52:08 +0200

    Fix indentation
    Remove useless comments, fix typo
    Align member declaration
    Reduce indentation levels
    consistent bracing style for if statements.

--- examples/ServerDiscoverable/main.cpp
+++ examples/ServerDiscoverable/main.cpp
@@ -2,47 +2,41 @@
 #include <open62541client.h>
 #include <serverrepeatedcallback.h>
 /*!
-    This is an example server that registers with the discover y server
+This is an example server that registers with the discovery server
 */
-
-
 
 using namespace std;
 #define DISCOVERY_SERVER_ENDPOINT "opc.tcp://localhost:4850"
 
 // example discoverable server - give port and server name as arguments
 /*!
- * \brief The TestServer class
- */
+* \brief The TestServer class
+*/
 class TestServer : public Open62541::Server {
-        int _idx; // namespace index
-        UA_UInt64 _discoveryid;
-        //
-        Open62541::SeverRepeatedCallback _repeatedEvent; //
-        Open62541::Client _client;
+    int                               _idx; // namespace index
+    UA_UInt64                         _discoveryId;
+    Open62541::SeverRepeatedCallback  _repeatedEvent;
+    Open62541::Client                 _client;
 
-    public:
-        TestServer(int port)
-            : Open62541::Server(port),
-              _repeatedEvent(*this, 2000, [ & ](Open62541::SeverRepeatedCallback & s) {
+public:
+    TestServer(int port)
+        : Open62541::Server(port)
+        , _repeatedEvent(*this, 2000, [&](Open62541::SeverRepeatedCallback & s) {
             Open62541::NodeId nodeNumber(_idx, "Number_Value");
             int v = std::rand() % 100;
             Open62541::Variant numberValue(v);
-            cout << "_repeatedEvent called setting number value = " << v <<  endl;
+            cout << "_repeatedEvent called setting number value = " << v << endl;
             s.server().writeValue(nodeNumber, numberValue);
-        }) {
+        }) {}
 
-
-        }
-
-        void initialise(); // initialise the server before it runs but after it has been configured
+    void initialise(); // initialise the server before it runs but after it has been configured
 };
 /*!
- * \brief TestServer::initialise
- */
+* \brief TestServer::initialise
+*/
 void TestServer::initialise() {
     _idx = addNamespace("urn:test:test"); // create a name space
-    // Add a node and set its context to test context
+                                          // Add a node and set its context to test context
     Open62541::NodeId newFolder(_idx, "ServerItems");
     if (addFolder(Open62541::NodeId::Objects, "ServerItems", newFolder, Open62541::NodeId::Null)) {
         cout << "Create Number_Value" << endl;
@@ -66,35 +60,33 @@ void TestServer::initialise() {
                 cerr << "Registered with discovery server" << endl;
             }
         }
-        else
-        {
+        else {
             cerr << "Failed to connect with discovery server" << endl;
         }
-
     }
 }
 /*!
- * \brief main
- * \param argc
- * \param argv
- * \return
- */
+* \brief main
+* \param argc
+* \param argv
+* \return
+*/
 int main(int argc, char **argv) {
     if (argc != 3) {
         cerr << "Usage: ServerDiscoverable <port> <Server Name>" << endl;
+        return 0;
     }
-    else {
-        //
-        int port = ::atoi(argv[1]);
-        std::string name(argv[2]);
-        //
-        cerr << "Port " << port << " Name " << name << endl;
-        TestServer server(port);
-        server.setMdnsServerName(name);
-        server.setServerUri("Test Discoverable Server");
-        cerr << "Starting server" << endl;
-        server.start();
-        cerr << "Server Finished" << endl;
-    }
+
+    int port = ::atoi(argv[1]);
+    std::string name(argv[2]);
+
+    cerr << "Port " << port << " Name " << name << endl;
+    TestServer server(port);
+    server.setMdnsServerName(name);
+    server.setServerUri("Test Discoverable Server");
+    cerr << "Starting server" << endl;
+    server.start();
+    cerr << "Server Finished" << endl;
+
     return 0;
 }
--- examples/TestServer/main.cpp
+++ examples/TestServer/main.cpp
@@ -8,92 +8,85 @@ using namespace std;
 
 // example server
 class TestServer : public Open62541::Server {
-        int _idx; // namespace index
-        Open62541::SeverRepeatedCallback _repeatedEvent; //
-        TestMethod _method;
-        TestContext _context;
-        TestObject _object; // object
-    public:
-        TestServer() : _repeatedEvent(*this, 2000, [&](Open62541::SeverRepeatedCallback &s) {
-            Open62541::NodeId nodeNumber(_idx, "Number_Value");
-            int v = std::rand() % 100;
-            Open62541::Variant numberValue(v);
-            cout << "_repeatedEvent called setting number value = " << v <<  endl;
-            s.server().writeValue(nodeNumber,numberValue);
-        }),
-        _object(*this)
+    int                               _idx; // namespace index
+    Open62541::SeverRepeatedCallback  _repeatedEvent;
+    TestMethod                        _method;
+    TestContext                       _context;
+    TestObject                        _object;
 
-        {
+public:
+  TestServer()
+    : _repeatedEvent(*this, 2000, [&](Open62541::SeverRepeatedCallback &s) {
+        Open62541::NodeId nodeNumber(_idx, "Number_Value");
+        int v = std::rand() % 100;
+        Open62541::Variant numberValue(v);
+        cout << "_repeatedEvent called setting number value = " << v << endl;
+        s.server().writeValue(nodeNumber, numberValue);
+      })
+    , _object(*this)    {}
 
-        }
-
-        void initialise(); // initialise the server before it runs but after it has been configured
+  void initialise() override; // initialise the server before it runs but after it has been configured
 };
 
+//*****************************************************************************
+
 void TestServer::initialise() {
-    _idx = addNamespace("urn:test:test"); // create a name space
+    _idx = addNamespace("urn:test:test"); // create a namespace
+
     // Add a node and set its context to test context
     Open62541::NodeId newFolder(_idx,"ServerMethodItem");
-    if (addFolder(Open62541::NodeId::Objects, "ServerMethodItem", newFolder,Open62541::NodeId::Null)) {
-        // Add a string value to the folder
-        Open62541::NodeId variable(_idx, "String_Value");
-        Open62541::Variant v("A String Value");
-        if (!addVariable(newFolder, "String_Value", v, variable, Open62541::NodeId::Null, &_context)) {
-            cout << "Failed to add node " << Open62541::toString(variable)
-                 << " " <<  UA_StatusCode_name(lastError()) << endl;
-        }
-        else {
-            // attach value callbacks to this node
-            if (!_context.setValueCallback(*this, variable)) {
-                cout << "Failed to set value callback" << endl;
-            }
-        }
 
-        cout << "Create Number_Value" << endl;
-        Open62541::NodeId nodeNumber(_idx, "Number_Value");
-        Open62541::Variant numberValue(1);
-        if (!addVariable(Open62541::NodeId::Objects, "Number_Value", numberValue, nodeNumber, Open62541::NodeId::Null))
-        {
-            cout << "Failed to create Number Value Node " << endl;
-        }
+    if (!addFolder(Open62541::NodeId::Objects, "ServerMethodItem", newFolder, Open62541::NodeId::Null)) {
+      cout << "Failed to add folder " << " " << UA_StatusCode_name(lastError()) << endl;
+      return;
+    }
+    
+    // Add a string value to the folder
+    Open62541::NodeId variable(_idx, "String_Value");
+    Open62541::Variant v("A String Value");
 
-        //
-        // Start repeated event
-        //
-        _repeatedEvent.start();
-        //
-        // Create TestMethod node
-        //
-        Open62541::NodeId methodId(_idx, 12345);
-        if (_method.addServerMethod(*this, "TestMethod", newFolder, methodId, Open62541::NodeId::Null, _idx)) {
-            cout << "Added TestMethod - Adds two numbers together - call from client (e.g. UAExpert)" << endl;
-        }
-        else {
-            cout << "Failed to add method " << " " <<  UA_StatusCode_name(lastError()) << endl;
-        }
-        //
-        // Define an object type
-        //
-        Open62541::NodeId testType(_idx,"TestObjectType");
-        if(_object.addType(testType))
-        {
-            cout << "Added TestObject type" << endl;
-        }
-        else
-        {
-            cout << "Failed to create object type" << endl;
-        }
+    if (!addVariable(newFolder, "String_Value", v, variable, Open62541::NodeId::Null, &_context)) {
+        cout << "Failed to add node " << Open62541::toString(variable)
+              << " " <<  UA_StatusCode_name(lastError()) << endl;
+    }
+    // attach value callbacks to this node
+    else if (!_context.setValueCallback(*this, variable)) {
+        cout << "Failed to set value callback" << endl;
+    }
 
-        Open62541::NodeId exampleInstance(_idx,"ExampleInstance");
-        _object.addInstance("ExampleInstance",newFolder,exampleInstance);
+    // Add a Number value to the folder
+    cout << "Create Number_Value" << endl;
+    Open62541::NodeId nodeNumber(_idx, "Number_Value");
+    Open62541::Variant numberValue(1);
+    if (!addVariable(Open62541::NodeId::Objects, "Number_Value", numberValue, nodeNumber, Open62541::NodeId::Null)) {
+        cout << "Failed to create Number Value Node " << endl;
+    }
+
+    _repeatedEvent.start(); // Start repeated event
+
+    // Create TestMethod node
+    Open62541::NodeId methodId(_idx, 12345);
+    if (_method.addServerMethod(*this, "TestMethod", newFolder, methodId, Open62541::NodeId::Null, _idx)) {
+        cout << "Added TestMethod - Adds two numbers together - call from client (e.g. UAExpert)" << endl;
     }
     else {
-        cout << "Failed to add folder " << " " <<  UA_StatusCode_name(lastError()) << endl;
+        cout << "Failed to add method " << " " <<  UA_StatusCode_name(lastError()) << endl;
     }
 
+    // Define an object type
+    Open62541::NodeId testType(_idx,"TestObjectType");
+    if(_object.addType(testType)) {
+        cout << "Added TestObject type" << endl;
+    }
+    else {
+        cout << "Failed to create object type" << endl;
+    }
+
+    Open62541::NodeId exampleInstance(_idx,"ExampleInstance");
+    _object.addInstance("ExampleInstance",newFolder,exampleInstance);
 }
 
-int main(int/* argc*/, char **/*argv[]*/) {
+int main(int, char**) {
     TestServer server;
     cerr << "Starting server" << endl;
     server.start();
