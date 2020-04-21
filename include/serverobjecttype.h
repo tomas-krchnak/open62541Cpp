9d03b9ce5321a12fcc003a38c6e8413963774e1d
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-04-21 09:51:50 +0200

    Refact(ServerObjectType): Reorganization, Doxygen info, simplification

--- include/serverobjecttype.h
+++ include/serverobjecttype.h
@@ -19,157 +19,112 @@ namespace Open62541 {
 
 /**
  * The ServerObjectType class
- * Object type handling class - this is factory for object type - operates on a server instance
+ * Object type handling class
+ * this is a factory for object type - operates on a server instance
  * The NodeContext is the node life cycle manager
  */
 class UA_EXPORT ServerObjectType {
-    Server &_server;
-    std::string _name;
-    NodeId _typeId;
-    int _nameSpace = 2;
+    Server&     _server;
+    std::string _name;          /**< name of the Type */
+    NodeId      _typeId;        /**< node of the Type */
+    int         _nameSpace = 2; 
 
 public:
-    /**
-     * ServerObjectType
-     * @param s
-     */
-    ServerObjectType(Server &s, const std::string &n);
+    ServerObjectType(Server &s, const std::string &n)
+        : _server(s), _name(n)  {}
+    virtual ~ServerObjectType() {}
+
+    void    setNameSpace(int i) { _nameSpace = i; }
+    int     nameSpace()   const { return _nameSpace; }
+    Server &server()            { return _server; }
+    NodeId &typeId()            { return _typeId; }
+    const std::string &name()   { return _name; }
 
     /**
-     * ~ServerObjectType
+     * Add Base Object Type
+     * Creates an object type node with the BaseObject and HasSubType traits.
+     * It means this is the root node of an object hierarchy
+     * @param[in] name specify the display name of the object type
+     * @param[in,out] requestNodeId specify if a nodeId is already dedicated to hold the definition or if the nodeid must be created and returned.
+     *                if NodeId::Null a node is created and returned.
+     * @param context
+     * @return true on success, false otherwise
      */
-    virtual ~ServerObjectType();
+    bool addBaseObjectType(
+        const std::string&  name,
+        NodeId&             requestNodeId   = NodeId::Null,
+        NodeContext*        context         = nullptr);
 
     /**
-     * name
-     * @return 
-     */
-    const std::string &name() {
-        return _name;
-    }
-
-    /**
-     * nameSpace
-     * @return 
-     */
-    int nameSpace() const {
-        return _nameSpace;
-    }
-
-    /**
-     * setNameSpace
-     * @param i
-     */
-    void setNameSpace(int i) {
-        _nameSpace = i;
-    }
-
-    /**
-     * server
-     * @return 
-     */
-    Server &server() {
-        return _server;
-    }
-
-    /**
-     * typeId
-     * @return 
-     */
-    NodeId &typeId() {
-        return _typeId;
-    }
-
-    /**
-     * addBaseObjectType
-     * @param n
-     * @param typeId
-     * @return 
-     */
-    bool addBaseObjectType(const std::string &n, NodeId &requestNodeId = NodeId::Null, NodeContext *context = nullptr);
-
-    /**
-     * addObjectTypeVariable
-     * @param n
-     * @param parent
-     * @param nodeiD
+     * Add a Variable node to a parent object type node.
+     * @param name of the created Variable node
+     * @param parent specifies the node of the object type parent node
+     * @param nodeId 
      * @param mandatory
-     * @return 
+     * @return true on success, false otherwise
      */
-    template<typename T> bool addObjectTypeVariable(const std::string &n, NodeId &parent,
-                                                    NodeId &nodeId = NodeId::Null,
-                                                    NodeContext *context = nullptr,
-                                                    NodeId &requestNodeId = NodeId::Null, // usually want auto generated ids
-                                                    bool mandatory = true) {
+    template<typename T>
+    bool addObjectTypeVariable(
+        const std::string& name,
+        NodeId&      parent,
+        NodeId&      nodeId         = NodeId::Null,
+        NodeContext* context        = nullptr,
+        NodeId&      requestNodeId  = NodeId::Null, // usually want auto generated ids
+        bool         mandatory      = true) {
+
         T a{};
         Variant value(a);
 
         VariableAttributes var_attr;
         var_attr.setDefault();
-        var_attr.setDisplayName(n);
-        var_attr.setDescription(n);
+        var_attr.setDisplayName(name);
+        var_attr.setDescription(name);
         var_attr.get().accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
         var_attr.setValue(value);
         var_attr.get().dataType = value.get().type->typeId;
 
-        QualifiedName qn(_nameSpace, n.c_str());
         NodeId newNode;
         newNode.notNull();
 
-        if (_server.addVariableNode(requestNodeId,
-                                    parent,
-                                    NodeId::HasComponent,
-                                    qn,
-                                    NodeId::BaseDataVariableType,
-                                    var_attr,
-                                    newNode,
-                                    context)) {
-            if (mandatory) {
-                return _server.addReference(newNode,
-                                            NodeId::HasModellingRule,
-                                            ExpandedNodeId::ModellingRuleMandatory,
-                                            true);
-            }
-            if (!nodeId.isNull()) nodeId = newNode;
-            return true;
+        if (!_server.addVariableNode(
+                requestNodeId,
+                parent,
+                NodeId::HasComponent,
+                QualifiedName(_nameSpace, name.c_str()),
+                NodeId::BaseDataVariableType,
+                var_attr,
+                newNode,
+                context)){
+            UAPRINTLASTERROR(_server.lastError())
+            return false;
         }
-        UAPRINTLASTERROR(_server.lastError())
-        return false;
+
+        if (mandatory)
+            return setMandatory(newNode);
+
+        if (!nodeId.isNull())
+            nodeId = newNode;
+
+        return true;
     }
-
-    bool addObjectTypeFolder(const std::string &childName, NodeId &parent,
-                    NodeId &nodeId, NodeId &requestNodeId = NodeId::Null, bool mandatory = true)
-    {
-        NodeId newNode;
-        newNode.notNull();
-
-        if(_server.addFolder(parent,childName,newNode,requestNodeId))
-        {
-            if (mandatory) {
-                return _server.addReference(newNode,
-                                            NodeId::HasModellingRule,
-                                            ExpandedNodeId::ModellingRuleMandatory,
-                                            true);
-            }
-            if (!nodeId.isNull()) nodeId = newNode;
-            return true;
-        }
-        return false;
-    }
-
+    
     /**
      * addHistoricalObjectTypeVariable
      * @param n
      * @param parent
      * @param nodeiD
      * @param mandatory
-     * @return 
+     * @return true on success, false otherwise
      */
-    template<typename T> bool addHistoricalObjectTypeVariable(const std::string &n, NodeId &parent,
-                                                    NodeId &nodeId = NodeId::Null,
-                                                    NodeContext *context = nullptr,
-                                                    NodeId &requestNodeId = NodeId::Null, // usually want auto generated ids
-                                                    bool mandatory = true) {
+    template<typename T>
+    bool addHistoricalObjectTypeVariable(
+        const std::string &n,
+        NodeId &parent,
+        NodeId &nodeId = NodeId::Null,
+        NodeContext *context = nullptr,
+        NodeId &requestNodeId = NodeId::Null, // usually want auto generated ids
+        bool mandatory = true) {
+
         T a{};
         Variant value(a);
 
@@ -182,39 +137,29 @@ public:
         var_attr.get().dataType = value.get().type->typeId;
         var_attr.get().historizing = true;
 
-        QualifiedName qn(_nameSpace, n.c_str());
         NodeId newNode;
         newNode.notNull();
 
-        if (_server.addVariableNode(requestNodeId,
-                                    parent,
-                                    NodeId::HasComponent,
-                                    qn,
-                                    NodeId::BaseDataVariableType,
-                                    var_attr,
-                                    newNode,
-                                    context)) {
-            if (mandatory) {
-                return _server.addReference(newNode,
-                                            NodeId::HasModellingRule,
-                                            ExpandedNodeId::ModellingRuleMandatory,
-                                            true);
-            }
-            if (!nodeId.isNull()) nodeId = newNode;
-            return true;
+        if (!_server.addVariableNode(
+                requestNodeId,
+                parent,
+                NodeId::HasComponent,
+                QualifiedName(_nameSpace, n.c_str()),
+                NodeId::BaseDataVariableType,
+                var_attr,
+                newNode,
+                context)) {
+            UAPRINTLASTERROR(_server.lastError())
+            return false;
         }
-        UAPRINTLASTERROR(_server.lastError())
-        return false;
-    }
 
-    /**
-     * setMandatory
-     * @param n1
-     * @return 
-     */
-    bool setMandatory(NodeId &n1) {
-        return _server.addReference(n1, NodeId::HasModellingRule, ExpandedNodeId::ModellingRuleMandatory, true)
-                == UA_STATUSCODE_GOOD;
+        if (mandatory)
+            return setMandatory(newNode);
+
+        if (!nodeId.isNull())
+            nodeId = newNode;
+
+        return true;
     }
 
     /**
@@ -267,29 +212,36 @@ public:
 
     /**
      * addType
-     * @param server
-     * @param baseId
-     * @return 
+     * @param nodeId specify the base node of the type
+     * @return true on success, false otherwise
      */
-    virtual bool addType(NodeId &nodeId);  // base node of type
+    virtual bool addType(NodeId &nodeId);
 
     /**
-     * append
-     * @param parent
-     * @param nodeId
-     * @return 
+     * append a node to a parent as a derived object type
+     * @param parent the parent node object type
+     * @param nodeId the appended node
+     * @param requestNodeId
+     * @return true on success, false otherwise
      */
-    virtual bool append(NodeId &parent, NodeId &nodeId, NodeId &requestNodeId = NodeId::Null); // derived type
+    virtual bool append(
+        NodeId &parent,
+        NodeId &nodeId,
+        NodeId &requestNodeId = NodeId::Null); // derived type
 
     /**
      * addInstance
      * @param n
      * @param parent
      * @param nodeId
-     * @return 
+     * @return true on success, false otherwise
      */
-    virtual bool addInstance(const std::string &n, NodeId &parent,  NodeId &nodeId,
-                                NodeId &requestNodeId = NodeId::Null, NodeContext *context = nullptr);
+    virtual bool addInstance(
+        const std::string &name,
+        NodeId &parent,
+        NodeId &nodeId,
+        NodeId &requestNodeId = NodeId::Null,
+        NodeContext *context = nullptr);
 };
 
 } // namespace Open62541
