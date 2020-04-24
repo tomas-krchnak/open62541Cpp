cd796b7584fa6973df051963b61c342b3228cce3
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-04-24 13:19:52 +0200

    Refact(object): CCG NL 18 applied

--- src/open62541objects.cpp
+++ src/open62541objects.cpp
@@ -36,7 +36,7 @@ ExpandedNodeId   ExpandedNodeId::ModellingRuleMandatory(UA_EXPANDEDNODEID_NUMERI
 
 UA_BrowsePathTarget BrowsePathResult::nullResult = { UA_EXPANDEDNODEID_NUMERIC(0, 0), 0 };
 
-void Variant::fromAny(boost::any &a) {
+void Variant::fromAny(boost::any& a) {
     null(); // clear
     // get the type id as a hash code
     auto t = a.type().hash_code();
@@ -44,104 +44,103 @@ void Variant::fromAny(boost::any &a) {
         std::string v = boost::any_cast<std::string>(a);
         UA_String ss;
         ss.length = v.size();
-        ss.data = (UA_Byte *)(v.c_str());
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &ss, &UA_TYPES[UA_TYPES_STRING]);
+        ss.data = (UA_Byte*)(v.c_str());
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &ss, &UA_TYPES[UA_TYPES_STRING]);
     }
     else if (t == typeid(int).hash_code()) {
         int v = boost::any_cast<int>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_INT32]);
-
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_INT32]);
     }
     else if (t == typeid(char).hash_code()) {
         short v = short(boost::any_cast<char>(a));
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_INT16]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_INT16]);
     }
     else if (t == typeid(bool).hash_code()) {
         bool v = boost::any_cast<bool>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_BOOLEAN]);
     }
     else if (t == typeid(double).hash_code()) {
         double v = boost::any_cast<double>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_DOUBLE]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_DOUBLE]);
     }
     else if (t == typeid(unsigned).hash_code()) {
         unsigned v = boost::any_cast<unsigned>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_UINT32]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_UINT32]);
     }
     else if (t == typeid(long long).hash_code()) {
         long long v = boost::any_cast<long long>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_INT64]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_INT64]);
     }
     else if (t == typeid(unsigned long long).hash_code()) {
         unsigned long long v = boost::any_cast<unsigned long long>(a);
-        UA_Variant_setScalarCopy((UA_Variant *)ref(), &v, &UA_TYPES[UA_TYPES_UINT64]);
+        UA_Variant_setScalarCopy((UA_Variant*)ref(), &v, &UA_TYPES[UA_TYPES_UINT64]);
     }
 }
 
-std::string variantToString(UA_Variant &v) {
+std::string variantToString(UA_Variant& v) {
     std::string ret;
     switch (v.type->typeIndex) {
     case UA_TYPES_BOOLEAN: { // Boolean
-            ret = ((UA_Boolean *)(v.data)) ? "true" : "false";
+            ret = ((UA_Boolean*)(v.data)) ? "true" : "false";
         }
         break;
     case UA_TYPES_SBYTE: { // SByte
-            int i = *((char *)v.data);
+            int i = *((char*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_BYTE: { // Byte
-            unsigned i = *((unsigned char *)v.data);
+            unsigned i = *((unsigned char*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_INT16: { // Int16
-            int16_t i = *((int16_t *)v.data);
+            int16_t i = *((int16_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_UINT16: { // UInt16
-            uint16_t i = *((uint16_t *)v.data);
+            uint16_t i = *((uint16_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_INT32: { // Int32
-            int32_t i = *((int32_t *)v.data);
+            int32_t i = *((int32_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_UINT32: { // UInt32
-            uint32_t i = *((uint32_t *)v.data);
+            uint32_t i = *((uint32_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_INT64: { // Int64
-            int64_t i = *((int64_t *)v.data);
+            int64_t i = *((int64_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_UINT64: { // UInt64
-            uint32_t i = *((uint32_t *)v.data);
+            uint32_t i = *((uint32_t*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_FLOAT: { // Float
-            float i = *((float *)v.data);
+            float i = *((float*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_DOUBLE: { // Double
-            double i = *((double *)v.data);
+            double i = *((double*)v.data);
             ret = std::to_string(i);
         }
         break;
     case UA_TYPES_STRING: { // String
-            UA_String *p = (UA_String *)(v.data);
-            ret = std::string((const char *)p->data, p->length);
+            UA_String* p = (UA_String*)(v.data);
+            ret = std::string((const char*)p->data, p->length);
         }
         break;
     case UA_TYPES_DATETIME: { // DateTime
-            UA_DateTime *p = (UA_DateTime *)(v.data);
+            UA_DateTime* p = (UA_DateTime*)(v.data);
             UA_DateTimeStruct dts = UA_DateTime_toStruct(*p);
             char b[64];
             int l = sprintf(b, "%02u-%02u-%04u %02u:%02u:%02u.%03u, ",
@@ -150,8 +149,8 @@ std::string variantToString(UA_Variant &v) {
         }
         break;
     case UA_TYPES_BYTESTRING: { // ByteString
-            UA_ByteString *p = (UA_ByteString *)(v.data);
-            ret = std::string((const char *)p->data, p->length);
+            UA_ByteString* p = (UA_ByteString*)(v.data);
+            ret = std::string((const char*)p->data, p->length);
         }
         break;
     default:
@@ -164,7 +163,7 @@ std::string Variant::toString() {
     return variantToString(*(ref()));
 }
 
-std::string toString(const UA_NodeId &n) {
+std::string toString(const UA_NodeId& n) {
     std::string ret = std::to_string(n.namespaceIndex) + ":";
 
     switch (n.identifierType) {
@@ -173,7 +172,7 @@ std::string toString(const UA_NodeId &n) {
 
     case UA_NODEIDTYPE_BYTESTRING:
     case UA_NODEIDTYPE_STRING:
-        return ret + std::string((const char *)(n.identifier.string.data), n.identifier.string.length);
+        return ret + std::string((const char*)(n.identifier.string.data), n.identifier.string.length);
     case UA_NODEIDTYPE_GUID: {
         char b[45];
         int l = sprintf(b, "%08X:%04X:%04X[%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X]",
@@ -197,7 +196,7 @@ std::string toString(const UA_NodeId &n) {
     return std::string("Invalid Node Type");
 }
 
-bool UANodeTree::createPathFolders(UAPath &p, UANode *n, int level /*= 0*/) {
+bool UANodeTree::createPathFolders(UAPath& p, UANode* n, int level /*= 0*/) {
     bool ret = false;
     if (!n->hasChild(p[level])) {
         NodeId no;
@@ -218,7 +217,7 @@ bool UANodeTree::createPathFolders(UAPath &p, UANode *n, int level /*= 0*/) {
     return ret;
 }
 
-bool UANodeTree::createPath(UAPath &p, UANode *n, Variant &v, int level /*= 0*/) {
+bool UANodeTree::createPath(UAPath& p, UANode* n, Variant& v, int level /*= 0*/) {
     bool ret = false;
     if (!n->hasChild(p[level])) {
         if (level == int(p.size() - 1)) { // terminal node , hence value
@@ -249,7 +248,7 @@ bool UANodeTree::createPath(UAPath &p, UANode *n, Variant &v, int level /*= 0*/)
     return ret;
 }
 
-bool UANodeTree::setNodeValue(UAPath &p, Variant &v) {
+bool UANodeTree::setNodeValue(UAPath& p, Variant& v) {
     if (exists(p)) {
         return setValue(node(p)->data(), v); // easy
     }
@@ -262,30 +261,30 @@ bool UANodeTree::setNodeValue(UAPath &p, Variant &v) {
     return false;
 }
 
-bool UANodeTree::setNodeValue(UAPath &p, const std::string &child, Variant &v) {
+bool UANodeTree::setNodeValue(UAPath& p, const std::string& child, Variant& v) {
     p.push_back(child);
     bool ret = setNodeValue(p, v);
     p.pop_back();
     return ret;
 }
 
-bool UANodeTree::getNodeValue(UAPath &p, Variant &v) {
+bool UANodeTree::getNodeValue(UAPath& p, Variant& v) {
     v.null();
-    UANode *np = node(p);
+    UANode* np = node(p);
     if (np) { // path exist ?
         return getValue(np->data(), v);
     }
     return false;
 }
 
-bool UANodeTree::getNodeValue(UAPath &p, const std::string &child, Variant &v) {
+bool UANodeTree::getNodeValue(UAPath& p, const std::string& child, Variant& v) {
     p.push_back(child);
     bool ret = getNodeValue(p, v);
     p.pop_back();
     return ret;
 }
 
-void UANodeTree::printNode(UANode* pNode, std::ostream &os, int level) {
+void UANodeTree::printNode(UANode* pNode, std::ostream& os, int level) {
     if (!pNode) {
         return; // no node to print
     }
@@ -319,7 +318,7 @@ UA_StatusCode BrowserBase::browseIter(
     return UA_STATUSCODE_GOOD;
 }
 
-void BrowserBase::print(std::ostream &os) {
+void BrowserBase::print(std::ostream& os) {
     for (BrowseItem& item : _list) {
         std::string name;
         int         nsIdx;
@@ -332,7 +331,7 @@ void BrowserBase::print(std::ostream &os) {
     }
 }
 
-BrowseList::iterator BrowserBase::find(const std::string &s) {
+BrowseList::iterator BrowserBase::find(const std::string& s) {
     BrowseList::iterator i = _list.begin();
     for (i = _list.begin(); i != _list.end(); i++) {
         BrowseItem &b = *i;
@@ -359,7 +358,7 @@ std::string timestampToString(UA_DateTime date) {
     return  std::string(b,l);
 }
 
-std::string dataValueToString(UA_DataValue *value) {
+std::string dataValueToString(UA_DataValue* value) {
     std::stringstream os;
     os << "ServerTime:" <<  timestampToString(value->serverTimestamp) << " ";
     os << "SourceTime:" <<  timestampToString(value->sourceTimestamp) << " ";
