95febe2e3ff7f6f61a4e6b29019814be6f2d770c
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-04-28 09:34:50 +0200

    Refact(object): Code simplification
    switch case simplified.
    Mono-letter name arg and var renamed.
    Separator added.
    Indentation

--- src/open62541objects.cpp
+++ src/open62541objects.cpp
@@ -16,24 +16,26 @@
 namespace Open62541 {
 
 // Standard static nodes
-NodeId   NodeId::Null(0, 0);
-NodeId   NodeId::Objects(0, UA_NS0ID_OBJECTSFOLDER);
-NodeId   NodeId::Server(0, UA_NS0ID_SERVER);
-NodeId   NodeId::Organizes(0, UA_NS0ID_ORGANIZES);
-NodeId   NodeId::FolderType(0, UA_NS0ID_FOLDERTYPE);
-NodeId   NodeId::HasOrderedComponent(0, UA_NS0ID_HASORDEREDCOMPONENT);
-NodeId   NodeId::BaseObjectType(0, UA_NS0ID_BASEOBJECTTYPE);
-NodeId   NodeId::HasSubType(0, UA_NS0ID_HASSUBTYPE);
-NodeId   NodeId::HasModellingRule(0, UA_NS0ID_HASMODELLINGRULE);
-NodeId   NodeId::ModellingRuleMandatory(0, UA_NS0ID_MODELLINGRULE_MANDATORY);
-NodeId   NodeId::HasComponent(0, UA_NS0ID_HASCOMPONENT);
-NodeId   NodeId::HasProperty(0, UA_NS0ID_HASPROPERTY);
-NodeId   NodeId::BaseDataVariableType(0, UA_NS0ID_BASEDATAVARIABLETYPE);
-NodeId   NodeId::BaseEventType(0, UA_NS0ID_BASEEVENTTYPE);
+NodeId NodeId::Null(0, 0);
+NodeId NodeId::Objects(0, UA_NS0ID_OBJECTSFOLDER);
+NodeId NodeId::Server(0, UA_NS0ID_SERVER);
+NodeId NodeId::Organizes(0, UA_NS0ID_ORGANIZES);
+NodeId NodeId::FolderType(0, UA_NS0ID_FOLDERTYPE);
+NodeId NodeId::HasOrderedComponent(0, UA_NS0ID_HASORDEREDCOMPONENT);
+NodeId NodeId::BaseObjectType(0, UA_NS0ID_BASEOBJECTTYPE);
+NodeId NodeId::HasSubType(0, UA_NS0ID_HASSUBTYPE);
+NodeId NodeId::HasModellingRule(0, UA_NS0ID_HASMODELLINGRULE);
+NodeId NodeId::ModellingRuleMandatory(0, UA_NS0ID_MODELLINGRULE_MANDATORY);
+NodeId NodeId::HasComponent(0, UA_NS0ID_HASCOMPONENT);
+NodeId NodeId::HasProperty(0, UA_NS0ID_HASPROPERTY);
+NodeId NodeId::BaseDataVariableType(0, UA_NS0ID_BASEDATAVARIABLETYPE);
+NodeId NodeId::BaseEventType(0, UA_NS0ID_BASEEVENTTYPE);
 
-ExpandedNodeId   ExpandedNodeId::ModellingRuleMandatory(UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY));
+ExpandedNodeId ExpandedNodeId::ModellingRuleMandatory(UA_EXPANDEDNODEID_NUMERIC(0, UA_NS0ID_MODELLINGRULE_MANDATORY));
 
 UA_BrowsePathTarget BrowsePathResult::nullResult = { UA_EXPANDEDNODEID_NUMERIC(0, 0), 0 };
+
+//*****************************************************************************
 
 void Variant::fromAny(const boost::any& a) {
     null(); // clear
@@ -76,99 +78,92 @@ void Variant::fromAny(const boost::any& a) {
     }
 }
 
+//*****************************************************************************
+
 std::string variantToString(const UA_Variant& v) {
-    std::string ret;
     switch (v.type->typeIndex) {
     case UA_TYPES_BOOLEAN: { // Boolean
-            ret = ((UA_Boolean*)(v.data)) ? "true" : "false";
+            return ((UA_Boolean*)(v.data)) ? "true" : "false";
         }
-        break;
     case UA_TYPES_SBYTE: { // SByte
             int i = *((char*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_BYTE: { // Byte
             unsigned i = *((unsigned char*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_INT16: { // Int16
             int16_t i = *((int16_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_UINT16: { // UInt16
             uint16_t i = *((uint16_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_INT32: { // Int32
             int32_t i = *((int32_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_UINT32: { // UInt32
             uint32_t i = *((uint32_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_INT64: { // Int64
             int64_t i = *((int64_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_UINT64: { // UInt64
             uint32_t i = *((uint32_t*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_FLOAT: { // Float
             float i = *((float*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_DOUBLE: { // Double
             double i = *((double*)v.data);
-            ret = std::to_string(i);
+            return std::to_string(i);
         }
-        break;
     case UA_TYPES_STRING: { // String
             UA_String* p = (UA_String*)(v.data);
-            ret = std::string((const char*)p->data, p->length);
+            return std::string((const char*)p->data, p->length);
         }
-        break;
     case UA_TYPES_DATETIME: { // DateTime
             UA_DateTime* p = (UA_DateTime*)(v.data);
             UA_DateTimeStruct dts = UA_DateTime_toStruct(*p);
             char b[64];
             int l = sprintf(b, "%02u-%02u-%04u %02u:%02u:%02u.%03u, ",
                    dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
-            ret = std::string(b,l);
+            return std::string(b,l);
         }
-        break;
     case UA_TYPES_BYTESTRING: { // ByteString
             UA_ByteString* p = (UA_ByteString*)(v.data);
-            ret = std::string((const char*)p->data, p->length);
+            return std::string((const char*)p->data, p->length);
         }
-        break;
     default:
             break;
     }
-    return ret;
+    return "";
 }
+
+//*****************************************************************************
 
 std::string Variant::toString() {
     return variantToString(*(ref()));
 }
+
+//*****************************************************************************
 
 std::string timestampToString(UA_DateTime date) {
     UA_DateTimeStruct dts = UA_DateTime_toStruct(date);
     char b[64];
     int l = sprintf(b, "%02u-%02u-%04u %02u:%02u:%02u.%03u, ",
         dts.day, dts.month, dts.year, dts.hour, dts.min, dts.sec, dts.milliSec);
-    return  std::string(b, l);
+    return std::string(b, l);
 }
+
+//*****************************************************************************
 
 std::string dataValueToString(const UA_DataValue& value) {
     std::stringstream os;
@@ -179,30 +174,33 @@ std::string dataValueToString(const UA_DataValue& value) {
     return os.str();
 }
 
+//*****************************************************************************
+
 std::string toString(const UA_NodeId& n) {
     std::string ret = std::to_string(n.namespaceIndex) + ":";
 
     switch (n.identifierType) {
     case UA_NODEIDTYPE_NUMERIC:
         return ret + std::to_string(n.identifier.numeric);
-
     case UA_NODEIDTYPE_BYTESTRING:
     case UA_NODEIDTYPE_STRING:
         return ret + std::string((const char*)(n.identifier.string.data), n.identifier.string.length);
     case UA_NODEIDTYPE_GUID: {
         char b[45];
-        int l = sprintf(b, "%08X:%04X:%04X[%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X]",
-                        n.identifier.guid.data1,
-                        n.identifier.guid.data2,
-                        n.identifier.guid.data3,
-                        n.identifier.guid.data4[0],
-                        n.identifier.guid.data4[1],
-                        n.identifier.guid.data4[2],
-                        n.identifier.guid.data4[3],
-                        n.identifier.guid.data4[4],
-                        n.identifier.guid.data4[5],
-                        n.identifier.guid.data4[6],
-                        n.identifier.guid.data4[7]);
+        int l = sprintf(
+            b,
+            "%08X:%04X:%04X[%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X]",
+            n.identifier.guid.data1,
+            n.identifier.guid.data2,
+            n.identifier.guid.data3,
+            n.identifier.guid.data4[0],
+            n.identifier.guid.data4[1],
+            n.identifier.guid.data4[2],
+            n.identifier.guid.data4[3],
+            n.identifier.guid.data4[4],
+            n.identifier.guid.data4[5],
+            n.identifier.guid.data4[6],
+            n.identifier.guid.data4[7]);
 
         return ret + std::string(b, l);
     }
@@ -212,100 +210,117 @@ std::string toString(const UA_NodeId& n) {
     return std::string("Invalid Node Type");
 }
 
+//*****************************************************************************
+
 bool UANodeTree::createPathFolders(
-    const UAPath& p,
-    UANode*       n,
+    const UAPath& path,
+    UANode*       pNode,
     int           level /*= 0*/) {
     bool ret = false;
-    if (!n->hasChild(p[level])) {
+    if (!pNode->hasChild(path[level])) {
         NodeId no;
-        ret = addFolderNode(n->data(), p[level], no);
+        ret = addFolderNode(pNode->data(), path[level], no);
         if (ret) {
-            auto nn = n->add(p[level]);
+            auto nn = pNode->add(path[level]);
             if (nn) nn->setData(no);
         }
     }
 
     // recurse
-    n = n->child(p[level]);
+    pNode = pNode->child(path[level]);
     level++;
-    if (level < int(p.size())) {
-        ret = createPathFolders(p, n, level);
+    if (level < int(path.size())) {
+        ret = createPathFolders(path, pNode, level);
     }
 
     return ret;
 }
 
+//*****************************************************************************
+
 bool UANodeTree::createPath(
-    const UAPath&   p,
-    UANode*         n,
-    const Variant&  v,
+    const UAPath&   path,
+    UANode*         pNode,
+    const Variant&  val,
     int             level /*= 0*/) {
     bool ret = false;
-    if (!n->hasChild(p[level])) {
-        if (level == int(p.size() - 1)) { // terminal node , hence value
+    if (!pNode->hasChild(path[level])) {
+        if (level == int(path.size() - 1)) { // terminal node , hence value
             NodeId no;
-            ret = addValueNode(n->data(), p[level], no, v);
+            ret = addValueNode(pNode->data(), path[level], no, val);
             if (ret) {
-                if (auto nn = n->add(p[level]))
+                if (auto nn = pNode->add(path[level]))
                     nn->setData(no);
             }
         }
         else {
             NodeId no;
-            ret = addFolderNode(n->data(), p[level], no);
+            ret = addFolderNode(pNode->data(), path[level], no);
             if (ret) {
-                auto nn = n->add(p[level]);
+                auto nn = pNode->add(path[level]);
                 if (nn) nn->setData(no);
             }
         }
     }
 
     // recurse
-    n = n->child(p[level]);
+    pNode = pNode->child(path[level]);
     level++;
-    if (level < int(p.size())) {
-        ret = createPath(p, n, v, level);
+    if (level < int(path.size())) {
+        ret = createPath(path, pNode, val, level);
     }
 
     return ret;
 }
 
-bool UANodeTree::setNodeValue(const UAPath& p, const Variant& v) {
-    if (exists(p)) {
-        return setValue(node(p)->data(), v); // easy
+//*****************************************************************************
+
+bool UANodeTree::setNodeValue(const UAPath& path, const Variant& val) {
+    if (exists(path)) {
+        return setValue(node(path)->data(), val); // easy
     }
-    else if (p.size() > 0) {
+    else if (path.size() > 0) {
         // create the path and add nodes as needed
-        if (createPath(p, rootNode(), v)) {
-            return setValue(node(p)->data(), v);
+        if (createPath(path, rootNode(), val)) {
+            return setValue(node(path)->data(), val);
         }
     }
     return false;
 }
 
-bool UANodeTree::setNodeValue(UAPath p, const std::string& child, const Variant& v) {
-    p.push_back(child);
-    bool ret = setNodeValue(p, v);
-    p.pop_back();
+//*****************************************************************************
+
+bool UANodeTree::setNodeValue(
+    UAPath              path,
+    const std::string&  child,
+    const Variant&      val) {
+    path.push_back(child);
+    bool ret = setNodeValue(path, val);
+    path.pop_back();
     return ret;
 }
 
-bool UANodeTree::getNodeValue(const UAPath& p, Variant& v) {
-    v.null();
-    UANode* np = node(p);
+//*****************************************************************************
+
+bool UANodeTree::getNodeValue(const UAPath& path, Variant& val) {
+    val.null();
+    UANode* np = node(path);
     if (np) { // path exist ?
-        return getValue(np->data(), v);
+        return getValue(np->data(), val);
     }
     return false;
 }
 
-bool UANodeTree::getNodeValue(UAPath p, const std::string& child, Variant& v) {
-    p.push_back(child);
-    bool ret = getNodeValue(p, v);
-    p.pop_back();
+//*****************************************************************************
+
+bool UANodeTree::getNodeValue(UAPath path, const std::string& name, Variant& val) {
+    path.push_back(name);
+    bool ret = getNodeValue(path, val);
+    path.pop_back();
     return ret;
 }
+
+//*****************************************************************************
 
 void UANodeTree::printNode(const UANode* pNode, std::ostream& os, int level /*= 0*/) {
     if (!pNode) {
@@ -325,6 +340,8 @@ void UANodeTree::printNode(const UANode* pNode, std::ostream& os, int level /*= 
     }
 }
 
+//*****************************************************************************
+
 UA_StatusCode BrowserBase::browseIter(
     UA_NodeId   childId,
     UA_Boolean  isInverse, // reverse iteration (not supported)
@@ -341,6 +358,8 @@ UA_StatusCode BrowserBase::browseIter(
     return UA_STATUSCODE_GOOD;
 }
 
+//*****************************************************************************
+
 void BrowserBase::print(std::ostream& os) {
     for (BrowseItem& item : _list) {
         std::string name;
@@ -354,6 +373,8 @@ void BrowserBase::print(std::ostream& os) {
     }
 }
 
+//*****************************************************************************
+
 BrowseList::iterator BrowserBase::find(const std::string& s) {
     BrowseList::iterator i = _list.begin();
     for (i = _list.begin(); i != _list.end(); i++) {
@@ -363,13 +384,14 @@ BrowseList::iterator BrowserBase::find(const std::string& s) {
     return i;
 }
 
-void BrowserBase::process(UA_NodeId childId, UA_NodeId referenceTypeId) {
-    std::string s;
-    int i;
-    NodeId n;
-    n = childId;
-    if (browseName(n, s, i)) {
-        _list.push_back(BrowseItem(s, i, childId, referenceTypeId));
+//*****************************************************************************
+
+void BrowserBase::process(UA_NodeId node, UA_NodeId type) {
+    std::string name;
+    int nameSpace;
+    NodeId nodeCopy = node;
+    if (browseName(nodeCopy, name, nameSpace)) {
+        _list.push_back(BrowseItem(name, nameSpace, node, type));
     }
 }
 
