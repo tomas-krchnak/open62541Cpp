32e2cd8b5cf2729865ec9a2af4e3da132b1fb5d8
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-05-01 16:55:27 +0200

    Refact(example): Update TestServer object type example.

--- examples/TestServer/testobject.h
+++ examples/TestServer/testobject.h
@@ -10,21 +10,18 @@ class TestObject : public opc::ServerObjectType
 {
 public:
     TestObject(opc::Server& s) : ServerObjectType(s, "TestObject") {}
-
     bool addChildren(opc::NodeId& parent) override; // ServerObjectType
 };
 
 class DeviceObject : public opc::ServerObjectType {
 public:
-    DeviceObject(opc::Server& s) : ServerObjectType(s, "TestObject") {}
-
+    DeviceObject(opc::Server& s) : ServerObjectType(s, "DeviceObject") {}
     bool addChildren(opc::NodeId &parent) override; // ServerObjectType
 };
 
 class PumpObject : public opc::ServerObjectType {
 public:
-    PumpObject(opc::Server& s) : ServerObjectType(s, "TestObject") {}
-
+    PumpObject(opc::Server& s) : ServerObjectType(s, "PumpObject") {}
     bool addChildren(opc::NodeId& parent) override; // ServerObjectType
 };
 
