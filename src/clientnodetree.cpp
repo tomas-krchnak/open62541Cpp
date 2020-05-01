c4452f8df6b317ea15ccf3b49c4672ac95036743
Jean-Joël Borter <jeanjoel.borter@gmail.com>
2020-05-01 17:03:02 +0200

    Refact(client): ClientNodeTree.cpp file added.

---
+++ src/clientnodetree.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2017 -  B. J. Hill
+ *
+ * This file is part of open62541 C++ classes. open62541 C++ classes are free software: you can
+ * redistribute it and/or modify it under the terms of the Mozilla Public
+ * License v2.0 as stated in the LICENSE file provided with open62541.
+ *
+ * open62541 C++ classes are distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.
+ */
+#include "clientnodetree.h"
+
+namespace Open62541 {
+
+} // namespace Open62541
