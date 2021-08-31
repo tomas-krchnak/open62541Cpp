34a6c5f71c237dad7e6b1686996bfe832dfa3812
Stefan Profanter <stefan.profanter@agile-robots.com>
2021-08-31 13:17:44 +0200

    fix: initialize object before copy to avoid memory error

--- examples/HistorianServer/testobject.h
+++ examples/HistorianServer/testobject.h
@@ -11,9 +11,12 @@ class TestObject : public opc::ServerObjectType
 public:
     TestObject(opc::Server &s) : ServerObjectType(s,"TestObject") {}
 
-    bool addChildren(const opc::NodeId &parent) override {
-        opc::NodeId top = addObjectTypeVariable<double>("Seme", parent);
-        opc::NodeId bot = addObjectTypeVariable<double>("Uke", top);
+    bool addChildren(const opc::NodeId& parent) override
+    {
+        opc::NodeId n;
+        opc::NodeId a;
+        addObjectTypeVariable<double>("Current", parent, n.notNull());
+        addObjectTypeVariable<double>("Average", n, a.notNull());
         return true;
     }
 };
