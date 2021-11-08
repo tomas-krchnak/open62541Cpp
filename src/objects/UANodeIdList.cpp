e440c1e35e216e9208c6f651c26274a32a12cf79
Alexandru Dobinda <2427920+apollo2030@users.noreply.github.com>
2021-11-08 14:50:17 +0100

    Move UANodeIdList to its own files

---
+++ src/objects/UANodeIdList.cpp
@@ -0,0 +1,34 @@
+
+/*
+    Copyright (C) 2017 -  B. J. Hill
+
+    This file is part of open62541 C++ classes. open62541 C++ classes are free software: you can
+    redistribute it and/or modify it under the terms of the Mozilla Public
+    License v2.0 as stated in the LICENSE file provided with open62541.
+
+    open62541 C++ classes are distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+    A PARTICULAR PURPOSE.
+*/
+#include <open62541cpp/objects/UANodeIdList.h>
+#include "open62541/types_generated_handling.h"
+
+namespace Open62541 {
+
+UANodeIdList::~UANodeIdList()
+{
+    for (auto& node : *this) {
+        UA_NodeId_deleteMembers(&node);  // delete node data
+    }
+}
+
+//*****************************************************************************
+
+void UANodeIdList::put(const UA_NodeId& node)
+{
+    UA_NodeId copy;  // deep copy
+    UA_NodeId_init(&copy);
+    UA_NodeId_copy(&node, &copy);
+    push_back(copy);
+}
+}  // namespace Open62541
