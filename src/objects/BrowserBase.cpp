5732bd373898ab99ba3b36e12066d29c54290618
Alexandru Dobinda <2427920+apollo2030@users.noreply.github.com>
2021-11-08 17:21:14 +0100

    Move BrowserBase to its own files

---
+++ src/objects/BrowserBase.cpp
@@ -0,0 +1,73 @@
+
+/*
+    Copyright (C) 2017 -  B. J. Hill
+
+    This file is part of open62541 C++ classes. open62541 C++ classes are free software: you can
+    redistribute it and/or modify it under the terms of the Mozilla Public
+    License v2.0 as stated in the LICENSE file provided with open62541.
+
+    open62541 C++ classes are distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+    A PARTICULAR PURPOSE.
+*/
+#include <open62541cpp/objects/BrowserBase.h>
+#include <open62541cpp/objects/NodeId.h>
+#include <open62541cpp/objects/StringUtils.h>
+#include <sstream>
+
+namespace Open62541 {
+
+UA_StatusCode BrowserBase::browseIter(UA_NodeId childId,
+                                      UA_Boolean isInverse,  // reverse iteration (not supported)
+                                      UA_NodeId referenceTypeId,
+                                      void* handle)
+{
+
+    // node iterator for browsing
+    if (isInverse) {
+        return UA_STATUSCODE_GOOD;  // TO DO what does this do?
+    }
+    if (auto p = (BrowserBase*)handle) {
+        p->process(childId, referenceTypeId);  // process record
+    }
+    return UA_STATUSCODE_GOOD;
+}
+
+//*****************************************************************************
+
+void BrowserBase::print(std::ostream& os)
+{
+    for (BrowseItem& item : _list) {
+        std::string name;
+        int nsIdx;
+        NodeId node = item.nodeId;  // copy i.childId, since browseName can modify it.
+        if (browseName(node, name, nsIdx)) {
+            os << toString(item.nodeId) << " ns:" << item.nameSpace << ": " << item.name
+               << " Ref:" << toString(item.type) << std::endl;
+        }
+    }
+}
+
+//*****************************************************************************
+
+BrowseItem* BrowserBase::find(const std::string& name)
+{
+
+    for (auto& item : _list) {
+        if (item.name == name)
+            return &item;
+    }
+    return nullptr;
+}
+
+//*****************************************************************************
+
+void BrowserBase::process(const UA_NodeId& node, UA_NodeId type)
+{
+    std::string name;
+    int nameSpace;
+    if (browseName(node, name, nameSpace)) {
+        _list.push_back(BrowseItem(name, nameSpace, node, type));
+    }
+}
+}  // namespace Open62541
